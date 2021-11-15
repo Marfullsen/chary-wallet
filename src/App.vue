d7f81c7424730e407ae9b6a0c13a450fef56c855
Francisco Marfull <marfullsen@gmail.com>
2021-11-15 02:19:35 -0300

    Feature: Desktop and Mobile navbars added.

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,90 @@
 <template>
-  <router-view/>
-  <div id="nav">
-    <router-link to="/">Move</router-link> |
-    <router-link to="/recent">Recent</router-link> |
-    <router-link to="/history">History</router-link>
+<div class="nav desktop">
+    <router-link to="/history"
+      ><Icon icon="simple-line-icons:notebook" width="30"
+    />&nbsp;History</router-link>
+    <router-link to="/"
+      ><Icon icon="simple-line-icons:wallet" width="40"
+    />&nbsp;Home</router-link>
+    <router-link to="/recent"
+      ><Icon icon="simple-line-icons:hourglass" width="25"
+    />&nbsp;Recent</router-link>
+  </div>
+  <router-view />
+  <div id="nav" class="nav mobile">
+    <router-link to="/history"
+      ><Icon icon="simple-line-icons:notebook" width="30"
+    /></router-link>
+    <router-link to="/"
+      ><Icon icon="simple-line-icons:wallet" width="40"
+    /></router-link>
+    <router-link to="/recent"
+      ><Icon icon="simple-line-icons:hourglass" width="25"
+    /></router-link>
   </div>
 </template>
 
+<script>
+import { Icon } from '@iconify/vue';
+
+export default {
+  components: {
+    Icon,
+  },
+};
+</script>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+.nav {
+  display: flex;
+  justify-content: space-evenly;
+  padding: 15px;
+  background: white;
 }
 
-#nav a {
+.desktop {
+  display: none;
+}
+
+.desktop a {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+}
+
+.mobile {
+  position: -webkit-sticky; /* Safari */
+  position: sticky;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  border-top: solid 1px;
+}
+
+.nav a {
   font-weight: bold;
   color: #2c3e50;
 }
 
-#nav a.router-link-exact-active {
+.nav a.router-link-exact-active {
   color: #42b983;
+}
+
+@media (min-width: 1024px) {
+  .desktop {
+    display: flex;
+  }
+  .mobile {
+    display: none;
+  }
 }
 </style>
