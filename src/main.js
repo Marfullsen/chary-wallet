e57893fe9ccfdd818bcbdfc2477953387fb1e72f
Francisco Marfull <marfullsen@gmail.com>
2021-11-16 21:31:34 -0300

    Feature: Global variable amount added.

--- src/main.js
+++ src/main.js
@@ -6,5 +6,6 @@ const app = createApp(App);
 const balance = localStorage.getItem('balance') ? Number(localStorage.getItem('balance')) : 0;
 
 app.config.globalProperties.$balanceGlobal = ref(balance);
+app.config.globalProperties.$amount = ref(0);
 
 app.use(router).mount('#app');
