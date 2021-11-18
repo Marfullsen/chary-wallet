7eb9c8a8a947d6bad4def2808da47bff12d00e96
Francisco Marfull <marfullsen@gmail.com>
2021-11-17 21:34:36 -0300

    Feature: Append a message to transactions waiting 2 seconds when pressing the add btn.

--- src/components/MoneyMenu.vue
+++ src/components/MoneyMenu.vue
@@ -13,7 +13,13 @@
         "
         v-text="'$' + amount"
       ></label>
-      <Icon icon="simple-line-icons:check" @click="increase" color="#118c4f" width="70" />
+      <Icon
+        icon="simple-line-icons:check"
+        @mousedown="onClick_or_LongPress"
+        @mouseup="onClick_or_LongPress"
+        color="#118c4f"
+        width="70"
+      />
     </div>
     <money-bar :value="20000" color="#D24C29" type="bill"></money-bar>
     <money-bar :value="10000" color="#2D639F" type="bill"></money-bar>
@@ -40,31 +46,55 @@ export default {
     return {
       amount: this.$amount,
       balance: this.$balanceGlobal,
+      startClick: 0,
     };
   },
   methods: {
-    increase() {
+    onClick_or_LongPress(e) {
       if (this.amount) {
-        // Update global value 'balance'.
-        this.balance += this.amount;
-        localStorage.setItem('balance', this.balance);
-
-        // Update global movements data.
-        let now = new Date(Date.now());
-        now = now.toLocaleString();
-        const transaction = [now, '', this.amount];
-        const movementsData = localStorage.getItem('movementsData');
-        let movements = JSON.parse(movementsData);
-        movements.push(transaction);
-        movements = JSON.stringify(movements);
-        localStorage.setItem('movementsData', movements);
-
-        // Reset the accumulator.
-        this.amount = 0;
+        if (e.type === 'mousedown') {
+          this.startClick = e.timeStamp;
+        } else if (e.type === 'mouseup' && this.startClick > 0) {
+          if (e.timeStamp - this.startClick > 500) {
+            const transactionMessage = this.appendMessage();
+            this.DoTransaction(transactionMessage);
+          } else {
+            this.DoTransaction();
+          }
+        }
       }
+    },
+    DoTransaction(transactionMessage = '') {
+      // Update global value 'balance'.
+      this.balance += this.amount;
+      localStorage.setItem('balance', this.balance);
+
+      // Update global movements data.
+      let now = new Date(Date.now());
+      now = now.toLocaleString();
+      const transaction = [now, transactionMessage, this.amount];
+      const movementsData = localStorage.getItem('movementsData');
+      let movements = JSON.parse(movementsData);
+      movements.push(transaction);
+      movements = JSON.stringify(movements);
+      localStorage.setItem('movementsData', movements);
+
+      // Reset the accumulator.
+      this.amount = 0;
     },
     cancel() {
       this.amount = 0;
+    },
+    appendMessage() {
+      // eslint-disable-next-line no-alert
+      const person = prompt('Transaction message (Optional)');
+      let text = '';
+      if (person == null || person === '') {
+        text = '';
+      } else {
+        text = `${person}`;
+      }
+      return text;
     },
     lookForPreviousData() {
       const movementsData = localStorage.getItem('movementsData');
