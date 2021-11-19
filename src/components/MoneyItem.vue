9200f715b42e2e0d49fc98bdc857639c608733dc
Francisco Marfull <marfullsen@gmail.com>
2021-11-18 22:31:41 -0300

    Feature: New design for the bills and coins UI/UX.

---
+++ src/components/MoneyItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="centrado">
+    <Icon
+      class="pointer"
+      @click="reduceAmount"
+      icon="bi:node-minus-fill"
+      :color="color"
+      width="80"
+      :rotate="2"
+    />
+    <img :src="img_src" :alt="img_src" :class="Number(value) < 1000 ? 'coin' : 'bill'">
+    <Icon
+      class="pointer"
+      @click="increaseAmount"
+      icon="bi:node-plus-fill"
+      :color="color"
+      width="80"
+    />
+  </div>
+</template>
+
+<script>
+import { Icon } from '@iconify/vue';
+
+export default {
+  props: {
+    color: String,
+    img_src: String,
+    value: Number,
+  },
+  components: {
+    Icon,
+  },
+  data() {
+    return {
+      amount: this.$amount,
+    };
+  },
+  methods: {
+    reduceAmount() {
+      this.amount -= this.value;
+    },
+    increaseAmount() {
+      this.amount += this.value;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.bill {
+  width: 150px;
+  padding: 15px;
+}
+
+.coin {
+  width: 75px;
+  padding: 15px;
+}
+</style>
