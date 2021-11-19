5a394aed8736f3568395a3828dd4fe8f54848021
Francisco Marfull <marfullsen@gmail.com>
2021-11-19 17:31:35 -0300

    Refactor: Balance Title relocated to the MoneyMenu.vue component.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,15 @@
 <template>
   <div>
-    <h1 style="margin: 0;">Balance</h1>
-    <balance-label></balance-label>
     <money-menu></money-menu>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import BalanceLabel from '@/components/BalanceLabel.vue';
 import MoneyMenu from '../components/MoneyMenu.vue';
 
 export default {
   name: 'Home',
   components: {
-    BalanceLabel,
     MoneyMenu,
   },
 };
