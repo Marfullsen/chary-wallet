f1cc81ab6d72e7b2be32075ee1dfbfe6e983d19c
Francisco Marfull <marfullsen@gmail.com>
2021-11-24 01:34:13 -0300

    Feature: INPUT of date format function changed to DD-MM-YYYY.

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,8 @@
 export default {
-  toDate(dateString) {
-    // dateString example could be '31-12-2021 01:02:03'
-    const reggie = /(\d{2})-(\d{2})-(\d{4}) (\d{2}):(\d{2}):(\d{2})/;
-    const [, day, month, year, hours, minutes, seconds] = reggie.exec(dateString);
-    const dateObject = new Date(year, month - 1, day, hours, minutes, seconds);
+  to_YY_MM_DD__Date(dateString) {
+    const reggie = /(\d{2})-(\d{2})-(\d{4})/;
+    const [, day, month, year] = reggie.exec(dateString);
+    const dateObject = new Date(year, month - 1, day);
     return dateObject;
   },
 };
