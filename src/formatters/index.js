22db028957e12823e8feb501466a172f4a5d0f49
Francisco Marfull <marfullsen@gmail.com>
2021-11-25 15:02:42 -0300

    Hotfix: Regex updated for Android Browsers compatibility.

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,6 +1,6 @@
 export default {
   to_YY_MM_DD__Date(dateString) {
-    const reggie = /(\d{2})-(\d{2})-(\d{4})/;
+    const reggie = /(\d{2}).(\d{2}).(\d{4})/;
     const [, day, month, year] = reggie.exec(dateString);
     const dateObject = new Date(year, month - 1, day);
     return dateObject;
