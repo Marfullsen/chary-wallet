a84077903f5da7901ce7dfbfa21da3de80ed437c
Francisco Marfull <marfullsen@gmail.com>
2022-06-28 21:58:53 -0400

    Refactor: settings for deploy improved.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env sh
 
+# Current date
+timestamp=$(date +%Y-%m-%d' '%H:%M)
+
 # abort on errors
 set -e
+
+# Get rid of CRLF problems
+npm run lint -- --fix
 
 # build
 npm run build
@@ -14,7 +20,7 @@ cp index.html 404.html
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'deploy at $timestamp'
 
 git push -f https://github.com/Marfullsen/tidy-wallet.git master:gh-pages
 
